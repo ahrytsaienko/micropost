9cdd5fefe3530fb0e8f3c92f9f4a88190a3debaa
temsan89 <temsan89@gmail.com>
2017-12-30 14:58:22 +0200

    test example import modules

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-const greeting = 'Hello World';
-console.log(greeting);
+
+// CommonJS module Syntax
+// const person = require('./mymodule1');
+
+// ES2015 module
+// import { person, sayHello } from './mymodule2'
+// import * as mod from './mymodule2';
+import greeting from './mymodule2';
